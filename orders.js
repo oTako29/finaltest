415fe5a5fc750444f67b72075001a130c31bde7b
oTako29 <162576138+oTako29@users.noreply.github.com>
2024-09-26 08:34:12 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Ouvinte Legal',
+        productNumber: '85743',
+        paymentStatus: 'Devido',
+        status: 'Pendente'
+    },
+    {
+        productName: 'Barbeiro',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado(a)',
+        status: 'Recusado'
+    },
+    {
+        productName: 'Apoio a Inclusão',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
